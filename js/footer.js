ad838cae17e59fc51c67ddcc1778eecbd857867c
Alapan Bagchi <85439790+alapanbagchi@users.noreply.github.com>
2022-08-10 18:17:57 +0530

    About Page Fixed

---
+++ js/footer.js
@@ -0,0 +1,11 @@
+const footercontent = `<section class=" get_started">
+<div class=" title_wrap">
+    <h2>Start Learning from <br />our professionals today!</h2>
+</div>
+<form action="">
+    <input type="text" name="" placeholder="Enter your email">
+    <button type="submit">Get Stared Today</button>
+</form>
+</section>`
+
+document.querySelector('footer').innerHTML = footercontent
