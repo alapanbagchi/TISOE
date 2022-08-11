c2d6eec2997a7f9eb0722711930c4e9d940e2c2f
Alapan Bagchi <85439790+alapanbagchi@users.noreply.github.com>
2022-08-11 17:35:18 +0530

    Terms and Conditions Footer Link added

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,5 @@
-
+let GIT = '/TISOE'
+const url = window.location.href.split('/').filter(item => item != '').splice(0, 2).join('//') + GITHUBPROD
 const footercontent = `<section class=" get_started">
 <div class="animate__up title_wrap">
     <h2>Start Learning from <br />our professionals today!</h2>
@@ -6,6 +7,9 @@ const footercontent = `<section class=" get_started">
 <form class="animate__up animate__delay__400" action="">
     <button type="submit">Get Stared Today</button>
 </form>
+<div style="margin-top:42px" class="animate__up animate__delay__600">
+    <a href="${url}/termsandconditions.html" class="footerinfo">Tisoe LLC. - Terms and Conditions</a>
+</div>
 </section>`
 
 document.querySelector('footer').innerHTML = footercontent
