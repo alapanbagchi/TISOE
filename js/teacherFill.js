22740c5823ce63f1120cfead6c9a5fe8ed85933c
Alapan Bagchi <85439790+alapanbagchi@users.noreply.github.com>
2022-08-12 21:21:03 +0530

    Alapan

--- js/teacherFill.js
+++ js/teacherFill.js
@@ -8,7 +8,7 @@ teachers.forEach(teacher => {
     <div style="margin-right:32px" class="teacher_slides slides">
     <a  href="teachers/teacher.html?id=${teacher.name.toLowerCase().replace(' ','_')}">
     <div class="teacher_slide_image">
-        <img src="${teacher.avatar.split('/upload')[0] + '/upload/c_scale,w_400,h_550' + teacher.avatar.split('/upload')[1]}"
+        <img width="400" height="550" src="${teacher.avatar.split('/upload')[0] + '/upload/c_scale,w_400,h_550' + teacher.avatar.split('/upload')[1]}"
             alt="">
     </div>
 
